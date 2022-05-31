ee390b0adafe30a4575a55a6531a3edd679ea61a
Hery <thitphavanh.psv@gmail.com>
2022-06-01 06:30:17 +0700

    update

--- command-center/command-center.py
+++ command-center/command-center.py
@@ -5,12 +5,12 @@ GUI = Tk()
 GUI.geometry('500x350')
 GUI.title('Hacking Command Center')
 
-button1 = Button(GUI, text='Check Folder')
-button1.place(x = 50, y = 200)
 
-button2 = ttk.Button(GUI, text='Check Folder')
-button2.place(x = 200, y = 200)
+Frame1 = Frame(GUI)
+Frame1.place(x = 200, y = 200)
 
+Button1 = ttk.Button(Frame1, text='Check Folder')
+Button1.pack(ipadx=15, ipady=10)
 
 
 GUI.mainloop()
