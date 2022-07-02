27009d2d1dcd10ddc4122039e7804bff3e61d858
Hery <thitphavanh.psv@gmail.com>
2022-07-02 12:21:38 +0700

    update

--- trojan/encrypt_decrypt.py
+++ trojan/encrypt_decrypt.py
@@ -1,13 +1,23 @@
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
-print(key)
+print('KEY:',key)
 
 # Object fernet
 f = Fernet(key)
 
 text = 'ສະບາຍດີເຈົ້າ ນີ້ແມ່ນຂໍ້ຄວາມຈາກເຫີຍ'
 
+message = f.encrypt(text.encode('utf-8'))
+print('-------------Encrypt-------------')
+
+print(message.decode('utf-8'))
+
+print('---------------------------------')
+
+
+
+'''
 with open('homework.docx', 'rb') as file:
     docfile = file.read()
 
@@ -20,3 +30,5 @@ with open('hacker23.xxx', 'w') as file:
     file.write(message.decode('utf-8'))
 
 print('---------------------------------')
+
+'''
